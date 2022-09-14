6d77e7a46696abcd7273a2c5d6d8dff55ecec332
rishabhShukla <rishabhshukla7458@gmail.com>
2022-09-14 16:03:40 +0530

    Initial commit

---
+++ Test_01.py
@@ -0,0 +1,12 @@
+print('hello python !')
+print('I am trying some aithmetic operations in python')
+num1 = input('Enter first number : ')
+num2 = input('Enter second number : ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+print( "this is sum of given numbers :",sum)
+
+# subtract given numbers
+sub = float(num1) - float(num2)
+print("Difference b/w given numbers is :" , sub)
